7fef31f8163e61ae990b19bd0fee2cb2a51803a7
pdmxdd <pdmxdd@gmail.com>
2021-10-07 13:39:44 -0500

    tutorials/5/Navigating to hero details

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -12,16 +12,9 @@ export class HeroesComponent implements OnInit {
 
   heroes: Hero[] = [];
 
-  selectedHero?: Hero;
-
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   constructor(private heroService: HeroService, private messageService: MessageService) { }
